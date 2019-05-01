cf8f55da0748cf12df24f5648a4242af75589ec0
kapilsthakkar25 <40014158+kapilsthakkar25@users.noreply.github.com>
2019-05-01 11:04:11 +0600

    Update remove.sh

--- kube-dashboard/remove.sh
+++ kube-dashboard/remove.sh
@@ -6,4 +6,5 @@ ns=kube-system
 kubectl delete -n $ns -f kubernetes-dashboard.yaml
 kubectl delete -n $ns -f sa-admin.yml
 kubectl delete -n $ns -f dashboard-admin.yml
+kubectl delete clusterrolebinding kubernetes-dashboard -n kube-system --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
 
